8e0e61364be37b9161583bd607013c04f57de2dc
Sujal <sujalyadav91ind@gmail.com>
2025-05-30 08:49:07 +0530

    tested a function using doctest

---
+++ doctests/add_two_numbers.py
@@ -0,0 +1,11 @@
+def add(num1: float, num2: float) -> float:
+    """
+    Return sum of two real numbers.
+
+    >>> add(1.2, 2.4)
+    3.6
+    >>> add(3.2, 9.1)
+    12.3
+    """
+
+    return round(num1 + num2,2)
---
+++ doctests/run_test.py
@@ -0,0 +1,4 @@
+import doctest
+import add_two_numbers
+
+doctest.testmod(add_two_numbers)
